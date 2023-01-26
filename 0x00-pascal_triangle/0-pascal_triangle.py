ac28f23847d18bbdc743eeabdbb0cbe9efe31793
esayyas <esayyasahmed1012@gmail.com>
2023-01-26 22:23:52 +0300

    0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    triangle = []
+    for i in range(1, n+1):
+        row = []
+        for j in range(i):
+            if j == 0 or j == i-1:
+                n = 1
+                row.append(n)
+            else:
+                n = triangle[i-2][j-1] + triangle[i-2][j]
+                row.append(n)
+        triangle.append(row)
+
+    return triangle
