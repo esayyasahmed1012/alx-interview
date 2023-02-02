9c16de99d3d2d2f819366ed325ec68a2e09259cf
esayyas <esayyasahmed1012@gmail.com>
2023-02-02 20:19:24 +0300

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,15 @@
+"""
+You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes
+"""
+
+
+
 def canUnlockAll(boxes):
+    """
+    boxes is a list of lists
+     A key with the same number as a box opens that box
+    You can assume all keys will be positive integers
+    """
     if type(boxes) is not list:
         return False
     elif (len(boxes)) == 0:
